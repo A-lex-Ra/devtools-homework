ebdb05e3fdd57799bc0363009e13afcde9f801be
pro100savant <pro100savant@yandex.ru>
2024-09-11 17:59:39 +0300

    Добавили время работы приложения

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import sys
+import time
 
 def default():
     print('Привет первый курс')
 
 def main():
+
+    start = time.time()
     default()
+    end = time.time()
+    
+    working_time = end - start
+    print(working_time)
 
 if __name__ == '__main__':
     main()
+    
